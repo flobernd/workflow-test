30766bae651c99ba3b5a77e5f878bb950f3e76bf
Florian Bernd <flobernd@users.noreply.github.com>
2022-08-17 15:48:36 +0200

    Initial commit

---
+++ WorkflowTest.App1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkflowTest.App1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");  
+            int x = 0;
+        }
+    }
+}
