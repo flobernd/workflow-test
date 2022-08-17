d496ed230132dd641badf6402a4e055a2995b2bd
Florian Bernd <flobernd@users.noreply.github.com>
2022-08-17 15:48:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# workflow-test
+GitHub Actions workflow test repository.
---
+++ WorkflowTest.App1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkflowTest.App1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");  
+        }
+    }
+}
